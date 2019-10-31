aa8e0455dd33fdfc9325c904782c606c04713b8e
FrozenIce0617 <prosdev087@gmail.com>
2019-10-31 12:19:26 -0400

    Add RootModal

---
+++ src/components/TelInput/RootModal.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+class RootModal extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.modalTarget = document.createElement("div");
+    this.modalTarget.className = "intl-tel-input iti-container";
+  }
+
+  componentDidMount() {
+    document.body.appendChild(this.modalTarget);
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.modalTarget);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <Fragment>{this.props.children}</Fragment>,
+      this.modalTarget
+    );
+  }
+}
+
+export default RootModal;
